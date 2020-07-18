c6abf5f1f6d96ecb72562d85e18714d7f9f88a7e
Raul Alonzo <ram2489@gmail.com>
2020-07-18 18:10:15 -0500

    Finished selection of axis to plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,25 +22,104 @@ const chart = svg
     .append("g")
     .attr("transform",`translate(${margin.left},${margin.top})`);
 
+let dataset = [];
+let povertyLabel;
+let healthcareLabel;
+let ageLabel;
+let smokesLabel;
+let householdLabel;
+let obeseLabel;
+let currentLabelX = "poverty";
+let currentLabelY = "healthcare";
 
-const plotChart = (data) => {
+
+
+
+const createDynamicChartLabels = () => {
+
+    povertyLabel = chart
+        .append("text")
+        .attr("id","poverty")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${chartWidth/2-margin.left/5},${(chartHeight+margin.top/3)+margin.bottom/6})`)
+        .text("Age Poverty (%)");
+
+    healthcareLabel = chart
+        .append("text")
+        .attr("id","healthcare")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${-margin.left/9},${chartHeight/2+margin.top*1.5}) rotate(-90)`)
+        .text("Lacks Healthcare (%)");
+
+    ageLabel =  chart
+        .append("text")
+        .attr("id","age")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${chartWidth/2-margin.left/5},${(chartHeight+margin.top/2)+margin.bottom/3})`)
+        .text("Age (Median)");
+
+    smokesLabel = chart
+        .append("text")
+        .attr("id","smokes")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${-margin.left/5},${chartHeight/2+margin.top*1.5}) rotate(-90)`)
+        .text("Smokes (%)");
+
+    householdLabel =  chart
+        .append("text")
+        .attr("id","income")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${chartWidth/2-margin.left/5},${(chartHeight+margin.top)+margin.bottom/3})`)
+        .text("Household Income (Median)");
+
+    obeseLabel = chart
+        .append("text")
+        .attr("id","obesity")
+        .classed("label inactive",true)
+        .attr("transform",`translate(${-margin.left/3+margin.right/22},${chartHeight/2+margin.top*1.5}) rotate(-90)`)
+        .text("Obese (%)");
+};
+
+const addActionsToLabels = () => {
+    [povertyLabel, ageLabel, householdLabel].forEach(l => createLabelActions(l,"x"));
+    [healthcareLabel, smokesLabel, obeseLabel].forEach(l => createLabelActions(l,"y"));
+}
+
+const createLabelActions = (label,axis) => {
+    label.on("click",() => {
+        if(axis === "x"){
+            currentLabelX = label.property("id");
+        } else {
+            currentLabelY = label.property("id");
+        }
+
+        plotChart(dataset,currentLabelX, currentLabelY);
+    });
+};
+
+const plotChart = (data,currentLabelX,currentLabelY) => {
+
 
     console.log(data);
 
+    chart.html("");
+    createDynamicChartLabels(dataset);
+    addActionsToLabels();
+
     data.forEach(d => {
-        d.healthcare = +d.healthcare;
-        d.poverty = +d.poverty;
+        d[currentLabelY] = +d[currentLabelY];
+        d[currentLabelX] = +d[currentLabelX];
     });
 
 
     const xScale = d3
         .scaleLinear()
-        .domain([d3.min(data, d => d.poverty), d3.max(data, d => d.poverty)+2])
+        .domain([d3.min(data, d => d[currentLabelX]), d3.max(data, d => d[currentLabelX])+2])
         .range([0,chartWidth])
         .nice();
     const yScale = d3
         .scaleLinear()
-        .domain([d3.min(data, d => d.healthcare), d3.max(data, d => d.healthcare)+2])
+        .domain([d3.min(data, d => d[currentLabelY]), d3.max(data, d => d[currentLabelY])+2])
         .range([chartHeight,0])
         .nice();
 
@@ -50,32 +129,30 @@ const plotChart = (data) => {
     const leftAxis = d3
         .axisLeft(yScale);
 
-    const circles =  chart
-        .selectAll("circles")
+    chart
+        .selectAll("circle")
         .data(data)
         .enter()
+        .append("g")
+        .classed("stateCircle",true)
         .append("circle")
-        .attr("data-poverty", d => d.poverty)
-        .attr("data-healthcare", d => d.healthcare)
-        .attr("cx", d => xScale(d.poverty))
-        .attr("cy", d => yScale(d.healthcare))
+        .attr("data-poverty", d => d[currentLabelX])
+        .attr("data-healthcare", d => d[currentLabelY])
+        .attr("cx", d => xScale(d[currentLabelX]))
+        .attr("cy", d => yScale(d[currentLabelY]))
         .attr("r","12")
         .attr("fill","rgb(142,192,213)");
 
-    const texts = chart
-        .selectAll("text")
-        .data(data)
-        .enter()
+    chart
+        .selectAll("g.stateCircle")
         .append("text")
+        .classed("abbr",true)
         .attr("fill","#fff")
         .attr("text-anchor","middle")
-        .attr("x", d => xScale(d.poverty))
-        .attr("y",d => yScale(d.healthcare)+2)
+        .attr("x", d => xScale(d[currentLabelX]))
+        .attr("y",d => yScale(d[currentLabelY])+2)
         .attr("dy","0.2em")
-        .text(d => {
-            console.log("Abbr", d.abbr);
-            return  d.abbr
-        });
+        .text(d => d.abbr);
 
     chart
         .append("g")
@@ -103,28 +180,16 @@ const plotChart = (data) => {
         .select("#y-axis")
         .selectAll(".tick:last-child")
         .remove();
-
-    chart
-        .append("text")
-        .attr("transform",`translate(${chartWidth/2-margin.left/5},${chartHeight+margin.bottom/3})`)
-        .attr("stroke","black")
-        .attr("stroke-width","1px")
-        .attr("font-size","1.3em")
-        .text("In Poverty (%)");
-
-    chart
-        .append("text")
-        .attr("transform",`translate(${-margin.left/9},${chartHeight/2+margin.top*1.5}) rotate(-90)`)
-        .attr("stroke","black")
-        .attr("stroke-width","1px")
-        .attr("font-size","1.3em")
-        .text("Lacks Healthcare (%)");
 }
 
-let dataset = []
+
 const init = async() => {
     dataset = await d3.csv(path).then(data => data);
-    plotChart(dataset);
+    try{
+        plotChart(dataset,currentLabelX,currentLabelY);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 init();
